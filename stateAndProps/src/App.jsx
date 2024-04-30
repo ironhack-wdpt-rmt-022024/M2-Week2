4b759413e0d32b28d8a2a2f7e439d00af4e23698
Joshua George <horheyinc8@gmail.com>
2024-04-30 21:34:52 +0200

    state and lists

--- stateAndProps/src/App.jsx
+++ stateAndProps/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { PizzaButton } from "./components/PizzaButton";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pizzaCount, setPizzaCount] = useState(0);
+  const [pizzaTypes, setPizzaTypes] = useState([
+    { id: 1, type: "pep", isGood: true },
+    { id: 2, type: "pinn", isGood: false },
+    { id: 3, type: "mush", isGood: false },
+  ]);
+  // this function is to add a pizza to the pizza count
+  const handleEatPizza = () => {
+    console.log("josh ate another pizza");
+    setPizzaCount(pizzaCount + 1);
+  };
+  //here you can use if/else, above the return
+  if (true) {
+    console.log("this is in the component but above the return");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Learning about useState and Lists</h2>
+      <h3>Joshs Pizza Count {pizzaCount}</h3>
+      <PizzaButton handleEatPizza={handleEatPizza} pizzaCount={pizzaCount} />
+      <h2>Pizza Types:</h2>
+      <ul>
+        {pizzaTypes.map((onePizzaType) => {
+          {
+            /* make sure to always use return inside of your maps!!!! */
+          }
+
+          return (
+            //ternary example with ? and :
+            /* <li key={onePizzaType.id}>
+              {onePizzaType.type} - {onePizzaType.isGood ? "Yum" : "Yuck"}
+            </li> */
+
+            //&& example
+            <li key={onePizzaType.id}>
+              {onePizzaType.type.length > 3 && onePizzaType.type}
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ stateAndProps/src/components/PizzaButton.jsx
@@ -0,0 +1,3 @@
+export const PizzaButton = ({ handleEatPizza }) => {
+  return <button onClick={handleEatPizza}>Eat </button>;
+};
